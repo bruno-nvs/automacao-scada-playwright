a513929b0f2169292b5929a7046cd806663f9224
brunonvs <bruno_p_neves@live.com>
2026-04-09 02:10:38 -0300

    fix: estabilização final do fluxo com pausas estratégicas

--- login_test.js
+++ login_test.js
@@ -5,19 +5,19 @@ Feature('Login');
 Scenario('Login com sucesso', ({ I }) => {
     I.amOnPage(process.env.URL_SISTEMA);
     I.switchTo('iframe#mainframe');
-    I.wait(5);
+    I.waitForElement('#username', 15);
     I.click('#username');
     I.fillField('#username', process.env.USUARIO_WEG);
     I.fillField('#password', process.env.SENHA_WEG);
     I.click('#login_button');
-    I.wait(5);
+    I.waitForText('Rio Grande DS', 20);
     I.click('Rio Grande DS');
     I.click('Ibirapuita');
-    I.wait(5);
+    I.waitForText('AEG02', 10);
     I.click('AEG02');
-    I.wait(5);
+    I.wait(2);
     I.click('Alarmes / registros');
-    I.wait(20); // Aguarda um pouco para garantir que a página carregue
+    I.wait(10);
     I.saveScreenshot('Login e Navegação Ok.png');
     I.say('🚀 Login, Navegação e Carregamento de Alarmes realizados com SUCESSO!');
-})
+});
